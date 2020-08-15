2766d734c4411160b58ff64ca1ce9d35bd8d9b4f
Jurick62 <Jurick62@mail.ru>
2020-08-15 21:31:26 +0300

    Corrected according to comments.

--- Test E-dostavka/EDostavka.cs
+++ Test E-dostavka/EDostavka.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTest_1
@@ -16,74 +9,79 @@ namespace SeleniumTest_1
     [TestFixture]
     public class Test
     {
+        const string edostavkaURL = "https://e-dostavka.by/";
+        const string tel = "375296502259";
+        const string pass = "123456Aa";
+
         IWebDriver driver = new ChromeDriver();
-        [OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        [OneTimeSetUp] 
         public void OneTimeSetUp()
         {
-            driver.Url = "https://e-dostavka.by/";
+            driver.Url = edostavkaURL;
             IWebElement loginButton = driver.FindElement(By.LinkText("Войти"));
             loginButton.Click();
         }
 
-        [OneTimeTearDown] //вызывается после завершения всех тестов
+        [OneTimeTearDown] 
         public void OneTimeTearDown()
         {
             driver.Quit();
         }
 
-        [SetUp] // вызывается перед каждым тестом
+        [SetUp] 
         public void SetUp()
         {
-            // Code here
         }
 
-        [TearDown] // вызывается после каждого теста
+        [TearDown] 
         public void TearDown()
         {
-            // Code here
         }
 
         [Test]
-        public void TEST_1()    //Проверяем переход на страницу авторизации
+        public void LOGIN_CHECK_TEST()
         {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.TitleContains("Единый аккаунт для всех проектов"));
             Assert.AreEqual("https://e-account.by/login/", driver.Url);
         }
 
         [Test]
-        public void TEST_2()    //Проверка страницы авторизации для физ. лиц. Позитивный тест.
+        public void AUTHENTICATION_TEST() 
         {
             //Проверяем, что активна ссылка "Физическое лицо"
             IWebElement person = driver.FindElement(By.ClassName("tab-1-active"));
             Assert.IsNotNull(person);
 
-            //Заполняем поле "Номер телефона". проверяем, что приведено к виду +ХХХ (ХХ) ХХХ-ХХ-ХХ и отображается верно
+            //Ввод логина. Проверка корректности отображения
             IWebElement loginName = driver.FindElement(By.CssSelector("input[type = 'tel']"));
             loginName.Click();
-            loginName.SendKeys("375296502259");
-            Assert.True(loginName.GetAttribute("value") == "+375 (29) 650-22-59");
+            loginName.SendKeys(tel);
+            Assert.AreEqual(loginName.GetAttribute("value"), "+375 (29) 650-22-59");
 
-            //Заполняем поле "Пароль". проверяем, что введенные символы не отображаются
+            //Ввод пароля. Проверка того, что введенные символы не отображаются
             IWebElement password = driver.FindElement(By.Name("Password"));
             password.Click();
-            password.SendKeys("123456Aa");
-            Assert.True(password.GetAttribute("type") == "password");
+            password.SendKeys(pass);
+            Assert.AreEqual(password.GetAttribute("type"), "password");
 
             //Кликаем кнопку "Войти"
             IWebElement submit = driver.FindElement(By.CssSelector("button[type = 'submit']"));
             submit.Click();
 
-            //Ждем открытия страницы
+            //Ожидание открытия страницы
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
             wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("user_fio")));
 
-            //Проверяем, что открылась старница https://e-dostavka.by/
+            //Проверка перехода на страницу https://e-dostavka.by/
             string myUrl = driver.Url;
             Assert.AreEqual("https://e-dostavka.by/", myUrl);
 
-            //И появилось ФИО пользователя
+            //Проверка корректности авторизации
             IWebElement userFIO = driver.FindElement(By.ClassName("user_fio"));
-            bool checkFIO = userFIO.Displayed;
-            Assert.True(userFIO.Displayed);
+            string checkFIO = userFIO.Text;
+            Assert.True(checkFIO == "Юрий\r\nТеуш");
+            
 
 
         }
